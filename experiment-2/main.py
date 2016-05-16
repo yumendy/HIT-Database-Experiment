5c9a09ce609c8abe1e3824968fb0bb1c5783c33b
yumendy <yumendy@163.com>
2016-05-16 13:55:18 +0800

    2016-5-16 update

--- experiment-2/main.py
+++ experiment-2/main.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import MySQLdb
 import argparse
 
@@ -7,6 +8,37 @@ USERNAME = 'test'
 PASSWORD = 'test'
 
 class App(object):
+
+    # 参加了项目编号为%PNO%的项目的员工号
+    _sql_templates_1 = \
+    '''
+    SELECT DISTINCT employee.essn
+    FROM employee
+    JOIN works_on ON employee.essn = works_on.essn
+    JOIN project ON project.pno = works_on.pno
+    WHERE project.pno='%s';
+    '''
+
+    # 参加了项目名为%PNAME%的员工名字
+    _sql_templates_2 = \
+    '''
+    SELECT DISTINCT employee.ename
+    FROM employee
+    JOIN works_on ON employee.essn = works_on.essn
+    JOIN project ON project.pno = works_on.pno
+    WHERE project.pname='%s';
+    '''
+
+    # 在%DNAME%工作的所有工作人员的名字和地址
+    _sql_templates_3 = \
+    '''
+    '''
+
+
+
+
+
+
     def __init__(self):
         try:
             self.connection = MySQLdb.connect(host=HOST, port=PORT, user=USERNAME, passwd=PASSWORD, charset='utf8')
@@ -17,9 +49,7 @@ class App(object):
         self.connection.select_db(db_name)
 
     def query(self, query_model, args_list):
-        result_tuple = tuple()
-        if query_model == '1':
-            result_tuple = self._function_1(args_list)
+        result_tuple = self._fill_template(query_model, args_list)
         self.print_result(result_tuple)
 
     def print_result(self, result_tuple):
@@ -43,26 +73,10 @@ class App(object):
         self.curs.close()
         self.connection.close()
 
-    def _function_1(self, args_list):
-        _sql_templates = \
-        '''
-        SELECT DISTINCT employee.essn
-        FROM employee
-        JOIN works_on ON employee.essn = works_on.essn
-        JOIN project ON project.pno = works_on.pno
-        WHERE project.pno='%s';
-        '''
-        sql = _sql_templates % tuple(args_list)
+    def _fill_template(self, q, args_list):
+        sql = self.__getattribute__('_sql_templates_' + str(q)) % tuple(args_list)
         return self._query(sql)
-    def _function_2(self, args_list):
-        _sql_templates = \
-        '''
-        SELECT DISTINCT employee.ename
-        FROM employee
-        JOIN works_on ON employee.essn = works_on.essn
-        JOIN project ON project.pno = works_on.pno
-        WHERE project.pname='%s';
-        '''
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -72,6 +86,7 @@ if __name__ == '__main__':
     args_console = parser.parse_args()
     query_model = args_console.q
     query_args_list = args_console.p
+    query_args_list = map(lambda item: item.decode('gbk'), query_args_list)
 
     app = App()
     app.select_db('company')
